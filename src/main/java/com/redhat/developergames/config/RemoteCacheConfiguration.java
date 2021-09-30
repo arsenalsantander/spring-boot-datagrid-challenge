a9507a8acca8f51201aafd6d0349dd4f5722f420
Francoise Ingrid Vieira Martins <francoise.martins@santander.com.br>
2021-09-30 16:28:51 -0300

    älteracoes ingrid

--- src/main/java/com/redhat/developergames/config/RemoteCacheConfiguration.java
+++ src/main/java/com/redhat/developergames/config/RemoteCacheConfiguration.java
@@ -1,29 +1,33 @@
 package com.redhat.developergames.config;
 
 import com.redhat.developergames.model.Weather;
+import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
 
-public class EmbeddedCacheConfiguration {
-    private final EmbeddedCacheManager cacheManager;
+@Configuration
+public class RemoteCacheConfiguration {
+
+    private final RemoteCacheManager cacheManager;
 
     @Autowired
     private Weather weather;
 
     @Autowired
-    public EmbeddedCacheConfiguration(EmbeddedCacheManager cacheManager) {
+    public RemoteCacheConfiguration(RemoteCacheManager cacheManager) {
         this.cacheManager = cacheManager;
     }
 
-    public void testeCache(EmbeddedCacheManager cacheManager) {
+    public void testeCache(RemoteCacheManager cacheManager) {
         cacheManager.getCache("testCache").put("testKey", "testValue");
         System.out.println("Received value from cache: " + cacheManager.getCache("testCache").get("testKey"));
     }
 
-    public void addValues(EmbeddedCacheManager cacheManager) {
+    public void addValues(RemoteCacheManager cacheManager) {
         cacheManager.getCache("testCache").put("Weather","{" + "temperature=" + weather.getTemperature() + ", condition=" + weather.getCondition() + ", city='" + weather.getCity() + '\'' + '}');
     }
+
+
 }
-
-
